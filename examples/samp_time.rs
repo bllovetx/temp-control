43a094b73b76373c012c59b4cea8ba89fff33335
Xuzhaoqiu Zeng <xzqzeng@gmail.com>
2022-08-05 23:50:22 +0800

    test sysclk and adc clock with oscilloscope

--- examples/samp_time.rs
+++ examples/samp_time.rs
@@ -1,8 +1,7 @@
-// ref: https://github.com/antoinevg/stm32f3-rust-examples/blob/master/src/stm32f3-02-dma.rs
 #![no_std]
 #![no_main]
 
-use cortex_m::asm;
+// use cortex_m::asm;
 use cortex_m_semihosting::hprintln;
 use panic_halt as _;
 use cortex_m_rt::entry;
@@ -10,10 +9,8 @@ use stm32f3xx_hal as hal;
 use hal::{
     adc,
     pac,
-    prelude::*, time::rate::Megahertz
+    prelude::*
 };
-
-
 
 
 #[entry]
@@ -21,15 +18,15 @@ fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
 
     let mut rcc = dp.RCC.constrain();
-    // TODO: Currently using 8MHz, switch to 72MHz for better conversion rate
     let clocks = rcc.cfgr
         .sysclk(64.MHz())
         .freeze(&mut dp.FLASH.constrain().acr);
+    // hprintln!("{:?}", clocks).unwrap();
 
     // prepare adc pa1(adc1_in2)
     let  unsafe_rcc = unsafe { &*pac::RCC::ptr() };
     unsafe_rcc.cfgr2.write(|w| w.adc12pres().no_clock());// ensure using ahb clock
-    let mut adc_common = adc::CommonAdc::new(dp.ADC1_2, &clocks, &mut rcc.ahb);
+    let adc_common = adc::CommonAdc::new(dp.ADC1_2, &clocks, &mut rcc.ahb);
     let mut adc = adc::Adc::new(
         dp.ADC1,
         adc::config::Config::default(),
@@ -37,7 +34,7 @@ fn main() -> ! {
         &adc_common,
     );
     let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
-    let mut pa1 = gpioa.pa1.into_analog(&mut gpioa.moder, &mut gpioa.pupdr);
+    let pa1 = gpioa.pa1.into_analog(&mut gpioa.moder, &mut gpioa.pupdr);
 
     // config cont mode
     adc.stop_conversion();
@@ -48,24 +45,32 @@ fn main() -> ! {
     adc.disable_interrupt(adc::Event::EndOfConversion);
     adc.disable_interrupt(adc::Event::EndOfSequence);
     adc.set_sequence_length(adc::config::Sequence::One);
+    adc.set_sample_time(&pa1, adc::config::SampleTime::Cycles1C5);
     let unsafe_adc1 = unsafe { &*pac::ADC1::ptr() };
     unsafe_adc1.sqr1.write(|w| unsafe{ w.sq1().bits(adc::channel::Id::Two.into()) });
     unsafe_adc1.isr.reset();
 
+    // prepare gpio (pa2)
+    let mut pa2 = gpioa
+          .pa2
+          .into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
+
     // record cycle num
-    let mut numb: u32 = 0;
+    let mut numb: u32 = 0;// modified in loop can't use shadowing
 
     // start adc
+    // hprintln!("prepare finished!").unwrap();
     unsafe_adc1.cr.write(|w| w.adstart().start());
 
     loop {
         while unsafe_adc1.isr.read().eoc().is_not_complete() {}
         // update state
         unsafe_adc1.isr.reset();
-        let numb = numb + 1;
+        numb = numb + 1;
         // print time per 1M cycles
-        if numb % 1_000_000 == 0 {
+        if numb % 1_000 == 0 {
             // dac output
+            pa2.toggle().unwrap();
         }
     }
 }
--- examples/test_gpio.rs
+++ examples/test_gpio.rs
@@ -10,20 +10,23 @@ use hal::{pac, prelude::*};
 #[entry]
 fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
-
     let mut rcc = dp.RCC.constrain();
+    let clocks = rcc.cfgr
+        .sysclk(8.MHz())
+        .freeze(&mut dp.FLASH.constrain().acr);
+
     // let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
     // let mut led = gpioe
     //       .pe13
     //       .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
 
     let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
-    let mut pa1 = gpioa
-          .pa1
+    let mut pa2 = gpioa
+          .pa2
           .into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
 
     loop {
-          pa1.toggle().unwrap();
+          pa2.toggle().unwrap();
           asm::delay(100_000);
     }
 }
